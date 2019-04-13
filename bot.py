d19f6da018bc02aff7ebc9d1d6d01b083cd8a8fd
Vezono2 <48598980+Vezono2@users.noreply.github.com>
2019-04-13 13:56:10 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -69,7 +69,7 @@ def dd(m):
     kb.add(*buttons1)
     kb.add(*buttons2)
     kb.add(*buttons3)
-    kb.add(types.InlineKeyboardButton(text='Окончить игру', callback_data='endgame '+str(number)))
+    kb.add(types.InlineKeyboardButton(text='И где же пасюк?', callback_data='endgame '+str(number)))
     polls.update({number:{
         'users':{},
         'dicks':dicks,
